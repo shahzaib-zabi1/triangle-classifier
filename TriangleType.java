8ae13bc51da6a8dbbf5241f28bc4dc669854b9cb
shahzaib <shahzaibxzabi@gmail.com>
2025-10-15 06:08:08 +0500

    Added triangle type checker code

---
+++ TriangleType.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TriangleType {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three sides of triangle: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a + b > c && a + c > b && b + c > a) {
+            if (a == b && b == c) {
+                System.out.println("Equilateral Triangle");
+            } else if (a == b || b == c || a == c) {
+                System.out.println("Isosceles Triangle");
+            } else {
+                System.out.println("Scalene Triangle");
+            }
+        } else {
+            System.out.println("Not a valid triangle");
+        }
+        sc.close();
+    }
+}
